904b293142696c86fe7c683b7c077b8550bc8a9c
marius.herciu23 <marius.herciu23@gmail.com>
2022-06-15 10:09:13 +0300

    returneaza obiectul elevPremiant

--- Program.cs
+++ Program.cs
@@ -7,17 +7,19 @@ namespace Ex3_Laborator6_
     {
         static void Main(string[] args)
         {
-            Elevi[] elev = new Elevi[3];
-            elev[0] = new Elevi("Popescu", "Razvan", new int[] { 5, 7, 9 });
-            elev[1] = new Elevi("Ilie", "Andrei", new int[] { 10, 4, 8, 9 });
-            elev[2] = new Elevi("Antonescu", "Ciprian", new int[] { 7, 9, 9 });
+            Elev[] elev = new Elev[3];
+            elev[0] = new Elev("Popescu", "Razvan", new int[] { 5, 7, 9 });
+            elev[1] = new Elev("Ilie", "Andrei", new int[] { 10, 4, 8, 9 });
+            elev[2] = new Elev("Antonescu", "Ciprian", new int[] { 7, 9, 9 });
 
             Catalog unsprezeceD = new Catalog("XI-D", elev);
 
             unsprezeceD.Tipareste();
 
-            int indexPremiant = unsprezeceD.GetPremiant();
-            Console.WriteLine($"Elevul premiant este {elev[indexPremiant].GetNumePremiant()}");
+            unsprezeceD.GetPremiantul();
+
+            Console.WriteLine("Elevul premiant este:");
+            unsprezeceD.GetPremiantul().Tipareste();
         }
     }
 }
